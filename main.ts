f612cc38e7205fc52ee50007995328473a234e2e
XJH <2324huangx@iesjoandaustria.org>
2026-01-16 14:28:30 +0100

    update 20260116

--- main.ts
+++ main.ts
@@ -1,17 +1,25 @@
+let Game_Start = false
 basic.forever(function () {
+    Game_Start = false
     basic.pause(randint(1000, 10000))
-    for (let index = 0; index < 1; index++) {
-        basic.showIcon(IconNames.Square)
-        basic.pause(1)
-        basic.showIcon(IconNames.SmallSquare)
-        basic.pause(1)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-        basic.pause(1)
+    Game_Start = true
+    if (true) {
+        for (let index = 0; index < 1; index++) {
+            music.setVolume(80)
+            music.play(music.builtinPlayableSoundEffect(soundExpression.soaring), music.PlaybackMode.UntilDone)
+            basic.showIcon(IconNames.Square)
+            basic.pause(1)
+            basic.showIcon(IconNames.SmallSquare)
+            basic.pause(1)
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+            basic.pause(1)
+            music.stopAllSounds()
+        }
     }
 })
