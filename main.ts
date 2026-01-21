44e999396877fd6df968fc26949d07f25456632d
XJH <2324huangx@iesjoandaustria.org>
2026-01-21 14:19:22 +0100

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,33 @@
+let B = 0
+let A = 0
 let Game_Start = false
-basic.forever(function () {
-    Game_Start = false
-    basic.pause(randint(1000, 10000))
-    Game_Start = true
-    if (true) {
-        for (let index = 0; index < 1; index++) {
-            music.setVolume(80)
-            music.play(music.builtinPlayableSoundEffect(soundExpression.soaring), music.PlaybackMode.UntilDone)
-            basic.showIcon(IconNames.Square)
-            basic.pause(1)
-            basic.showIcon(IconNames.SmallSquare)
-            basic.pause(1)
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . # . .
-                . . . . .
-                . . . . .
-                `)
-            basic.pause(1)
-            music.stopAllSounds()
+for (let index = 0; index < 100000000000; index++) {
+    for (let index = 0; index < 10000000000000000000; index++) {
+        Game_Start = false
+        basic.pause(randint(1000, 10000))
+        Game_Start = true
+        if (true) {
+            for (let index = 0; index < 1; index++) {
+                music.setVolume(80)
+                music.play(music.builtinPlayableSoundEffect(soundExpression.soaring), music.PlaybackMode.InBackground)
+                basic.showIcon(IconNames.Chessboard)
+                music.stopAllSounds()
+            }
         }
     }
-})
+    while (Game_Start) {
+        if (input.pinIsPressed(TouchPin.P1)) {
+            A += 1
+            basic.showNumber(A)
+            Game_Start = false
+        }
+        if (input.pinIsPressed(TouchPin.P2)) {
+            B += 1
+            basic.showNumber(B)
+            Game_Start = false
+        }
+        Game_Start = false
+        basic.pause(2000)
+        basic.clearScreen()
+    }
+}
